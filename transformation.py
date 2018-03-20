b82561e730f3404951303f483b42a5fc7c550a52
formation <formation@ubuntu.ubuntu-domain>
2018-03-20 23:22:23 +0100

    yup

--- transformation.py
+++ transformation.py
@@ -7,11 +7,11 @@ from pyproj import Proj,transform
 
 WGS = Proj(init='epsg:4326')
 WGS_UTM_32N = Proj(init='epsg:32632')
-long=10.1
-lat=36.8
-x2, y2 = transform(WGS,WGS_UTM_32N,long,lat)
-"""
+longitude=10.1
+latitude=36.8
+x2, y2 = transform(WGS,WGS_UTM_32N,longitude,latitude)
+
 print(x2)
 print(y2)
-"""
+
 
